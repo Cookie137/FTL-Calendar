00468140a95002ad365bd50d0c0382da3e813beb
Egor Kravchenko <70804710+Cookie137@users.noreply.github.com>
2025-07-21 22:08:37 +0300

    Added school version of project

---
+++ Calendar (Part 2).cpp
@@ -0,0 +1,215 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// =====================
+//        BACK-END
+// =====================
+
+// function №1
+// По номеру года определить, високосный он или нет.
+bool IsLeapYear(int year) {
+    return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
+}
+
+// function №2
+// По номеру года определить количество дней в году (365 или 366).
+int GetDaysInYear(int year) {
+    if (IsLeapYear(year)) {
+        return 366;
+    }
+    return 365;
+}
+
+// function №3
+// По номеру года и номеру месяца определить количество дней в месяце.
+int GetDaysInMonth(int year, int month) {
+    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+        return 31;
+    } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+        return 30;
+    } else if (IsLeapYear(year)) {
+        return 29;
+    }
+    return 28;
+}
+
+// function №4
+// Дана дата (день, месяц и год). Определить, сколько дней прошло до нее, начиная с 1 января данного года.
+int GetDaysSinceStartThisYear(int year, int month, int day) {
+    int result = 0;
+    for (int i = 1; i < month; i++) {
+        result += GetDaysInMonth(year, i);
+    }
+    result += day - 1;
+    return result;
+}
+
+// function №5
+// Дана дата (день, месяц и год). Определить, сколько дней прошло до нее, начиная с 1 января 1-го года.
+int GetDaysSinceChristBirthToYear(int year, int month, int day) {
+    int result = 0;
+    for (int i = 1; i < year; i++) {
+        result += GetDaysInYear(i);
+    }
+    result += GetDaysSinceStartThisYear(year, month, day);
+    return result;
+}
+
+// function №6
+// Даны две даты (день, месяц и год для каждой). Определить, сколько дней прошло между ними.
+int GetDaysBetweenTwoDates(int year_1, int month_1, int day_1, int year_2, int month_2, int day_2) {
+    int days_1, days_2;
+    days_1 = GetDaysSinceChristBirthToYear(year_1, month_1, day_1);
+    days_2 = GetDaysSinceChristBirthToYear(year_2, month_2, day_2);
+    return abs(days_1 - days_2);
+}
+
+// function №7
+// Даны две даты (день, месяц и год для каждой) и день недели первой даты (в виде целого числа от 1 до 7 или от 0 до 6).
+// Требуется определить день недели второй даты. Вторая дата может быть как позже, так и раньше первой.
+int GetWeekdayOfDateByWeekdayAnotherDate(int year_1, int month_1, int day_1, int year_2, int month_2, int day_2, int weekday_1) {
+    // weekday = [0, 6];
+    int weekday_2, days_1, days_2;
+    days_1 = GetDaysSinceChristBirthToYear(year_1, month_1, day_1);
+    days_2 = GetDaysSinceChristBirthToYear(year_2, month_2, day_2);
+    if (days_1 == days_2) {
+        return weekday_1;
+    } else if (days_1 > days_2) {
+        weekday_2 = (weekday_1 + (7 - (GetDaysBetweenTwoDates(year_1, month_1, day_1, year_2, month_2, day_2) % 7))) % 7;
+    } else if (days_1 < days_2) {
+        weekday_2 = (weekday_1 + GetDaysBetweenTwoDates(year_1, month_1, day_1, year_2, month_2, day_2)) % 7;
+    }
+    return weekday_2;
+}
+
+// =====================
+//       FRONT-END
+// =====================
+
+// Покрасить цвет текста консоли в любой цвет по RGB
+string PaintText(int r, int g, int b) {
+    string result = "\033[38;2;" + to_string(r) + ";" + to_string(g) + ";" + to_string(b) + "m";
+    return result;
+}
+
+// Получить название месяца по его номеру
+string GetMonthName(int number) {
+    string months [12] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+    return months[number - 1];
+}
+
+// Получить название дня недели по его номеру
+string GetWeekdayName(int number) {
+    string months [7] { "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su" };
+    return months[number];
+}
+
+// Центрировать текст по размеру
+string CenterText(string text, int width) {
+    int space_count = width / 2 - text.size() / 2;
+    string result;
+    for (int i = 0; i < space_count; i++) {
+        result += ' ';
+    }
+    result += text;
+    for (int i = 0; i < space_count; i++) {
+        result += ' ';
+    }
+    return result;
+}
+
+// Красит число в красный если в конкретный день праздник или выходной
+string IsTodayAHoliday(int month, int day, int weekday) {
+    if ((month == 1 && day >= 1 && day <= 8) ||
+        (month == 2 && day == 23) ||
+        (month == 3 && day == 8) ||
+        (month == 5 && day == 1) ||
+        (month == 5 && day == 9) ||
+        (month == 6 && day == 12) ||
+        (month == 7 && day == 15) ||
+        (month == 11 && day == 4) ||
+        (month == 12 && day == 31) ||
+        (weekday == 5 || weekday == 6))
+    {
+        return PaintText(255, 0, 0);
+    }
+    return PaintText(65, 105, 225);
+}
+
+/*
+// Вывести календарь по месяцу
+void DisplayMonth(int year, int month) {
+    int weekday = GetWeekdayOfDateByWeekdayAnotherDate(1970, 1, 1, year, month, 1, 3);
+    int days_in_month = GetDaysInMonth(year, month);
+    
+    cout << PaintText(0, 255, 127) << CenterText(GetMonthName(month) + " ", 20) << endl;
+    for (int i = 0; i < 7; i++) {
+        cout << PaintText(255, 140, 0) << GetWeekdayName(i) << " ";
+        for (int j = 1 + i - weekday; j <= days_in_month; j += 7) {
+            if (j < 1) {
+                cout << "   ";
+            } else {
+                cout << IsTodayAHoliday(month, j, i) << setw(2) << j << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+*/
+
+// Вывести календарь по 3 месяцам
+void Display3Months(int year, int quarter) {
+    int months[3], weekdays[3], days_in_months[3], temp_width;
+    for (int m = 0; m < 3; m++) {
+        months[m] = quarter * 3 + m + 1;
+        weekdays[m] = GetWeekdayOfDateByWeekdayAnotherDate(1970, 1, 1, year, months[m], 1, 3);
+        days_in_months[m] = GetDaysInMonth(year, months[m]);
+    }
+    for (int m = 0; m < 3; m++) {
+        cout << PaintText(0, 255, 127) << CenterText(GetMonthName(months[m]) + " ", 20) << "  ";
+    }
+    cout << endl;
+    for (int i = 0; i < 7; i++) {
+        for (int m = 0; m < 3; m++) {
+            temp_width = 0;
+            cout << PaintText(255, 140, 0) << GetWeekdayName(i) << " ";
+            for (int j = 1 + i - weekdays[m]; j <= days_in_months[m]; j += 7) {
+                temp_width++;
+                if (j < 1) {
+                    cout << "   ";
+                } else {
+                    cout << IsTodayAHoliday(months[m], j, i) << setw(2) << j << " ";
+                }
+            }
+            if (temp_width < 6) {
+                for (int j = temp_width; j < 6; j++) {
+                    cout << "   ";
+                }
+            }
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+// Вывести календарь по году
+void DisplayYear(int year) {
+    for (int i = 0; i < 4; i++) {
+        cout << endl;
+        Display3Months(year, i);
+    }
+}
+
+int main()
+{
+    int year;
+    do {
+        cout << "Введите год календаря > ";
+        cin >> year;
+    } while (int(year) < 1);
+    
+    DisplayYear(year);
+    
+    return 0;
+}
